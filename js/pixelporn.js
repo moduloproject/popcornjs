91fc157ef92351cb5ffe8749b7c47bd4dad591de
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-01-28 20:20:06 +0100

    #PixelPorn

---
+++ js/pixelporn.js
@@ -0,0 +1,24 @@
+function setAuto () {
+	PopcornJS.width = auto;
+	PopcornJS.height = auto;
+}
+function set480 () {
+	PopcornJS.width = 720;
+	PopcornJS.height = 480;
+}
+function set720 () {
+	PopcornJS.width = 1280;
+	PopcornJS.height = 720;
+}
+function set1080 () {
+	PopcornJS.width = 1920;
+	PopcornJS.height = 1080;
+}
+function set4K () {
+	PopcornJS.width = 3840;
+	PopcornJS.height = 2160;
+}
+function set8K () {
+	PopcornJS.width = 7680;
+	PopcornJS.height = 4320;
+}
