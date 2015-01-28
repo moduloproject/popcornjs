e058c401f01011a88283c3c036b9ceab198ea59e
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-01-28 20:47:04 +0100

    We all love dat js console

--- js/pixelporn.js
+++ js/pixelporn.js
@@ -1,24 +1,30 @@
 function setAuto () {
 	PopcornJS.width = auto;
 	PopcornJS.height = auto;
+	console.info("Auto");
 }
 function set480 () {
 	PopcornJS.width = 720;
 	PopcornJS.height = 480;
+	console.info("480px");
 }
 function set720 () {
 	PopcornJS.width = 1280;
 	PopcornJS.height = 720;
+	console.info("720px");
 }
 function set1080 () {
 	PopcornJS.width = 1920;
 	PopcornJS.height = 1080;
+	console.info("1080px");
 }
 function set4K () {
 	PopcornJS.width = 3840;
 	PopcornJS.height = 2160;
+	console.info("4K");
 }
 function set8K () {
 	PopcornJS.width = 7680;
 	PopcornJS.height = 4320;
+	console.info("8K");
 }
