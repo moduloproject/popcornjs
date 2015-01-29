8c4a990a7aaaf74236ea72aee65cf40ea22e973f
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-01-29 21:18:38 +0100

    Added fullscreen!

--- js/popcornjs.js
+++ js/popcornjs.js
@@ -67,3 +67,18 @@ window.addEventListener("load", function() {
 }, false);
 
 
+
+// Fullscreen
+
+function goFull () {
+    var elem = document.getElementById("popcornjs");
+    if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+    }
+}
